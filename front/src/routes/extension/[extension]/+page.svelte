096ffebbf84b3fd3dba54028f7daafaef528c8ac
Shogo Sawai <shogo.sawai@gmail.com>
2026-02-26 16:36:39 +0900

    :wheelchair: extension/[extension]/+page.svelte の警告修正
    Todo ID: 210
    Output: /div>
    	{/if}
    </div>
    ```
    ## 修正完了
    
    ### 変更内容
    
    | 箇所 | 変更内容 |
    |------|----------|
    | line 45 | 戻るアイコンの `<a>` 要素に `aria-label="戻る"` を追加 |
    
    ### 目的
    - アクセシビリティ警告の修正
    - アイコンのみのリンクにテキストラベルを追加し、スクリーンリーダーでの利用を可能にする

--- front/src/routes/extension/[extension]/+page.svelte
+++ front/src/routes/extension/[extension]/+page.svelte
@@ -45,6 +45,7 @@
 		<a
 			href="/extension/"
 			class="p-2 text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded transition"
+			aria-label="戻る"
 		>
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
